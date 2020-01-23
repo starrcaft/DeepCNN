8745db48b49141b35f7070818e957085de8c93ef
starrcaft <starrcaft@naver.com>
2020-01-24 00:36:54 +0900

    in Linear coding how to draw a line on graph

--- CNN/01.Linear/Linear.py
+++ CNN/01.Linear/Linear.py
@@ -25,7 +25,6 @@ plt.plot(x.numpy(), y.numpy(), 'o')
 plt.ylabel('y')
 plt.xlabel('x')
 
-plt.show()
 class Net(nn.Module):
     def __init__(self, input_size, output_size):
         super(Net, self).__init__()
@@ -40,3 +39,23 @@ torch.manual_seed(1)
 model = Net(1, 1)
 print(model)
 
+print(list(model.parameters()))
+
+w, b = model.parameters()
+
+def get_params():
+    return w[0][0].item(), b[0].item()
+
+def plot_fit(title):
+    plt.title = title
+    #wegith 과 bias
+    w1, b1 = get_params()
+    #-30~30
+    x1 = np.array([-30, 30])
+    #선형 추정선을 그려본다
+    y1 = w1 * x1 + b1
+    plt.plot(x1, y1, 'r')
+    plt.scatter(x, y)
+    plt.show()
+
+plot_fit('initialModel')
